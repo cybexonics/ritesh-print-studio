640d023d21ea60906ceeb9c4d5985972f1ee5949
cybexonics <cybexonicsitconsultants@gmail.com>
2025-03-02 14:18:32 +0530

    *

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://ritesh-print-studio.vercel.app/", // React app URL
+    origin: "*", // React app URL
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
